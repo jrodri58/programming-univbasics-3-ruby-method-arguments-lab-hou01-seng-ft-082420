bfb0ce0e8abc0a6701b8d3fe17befb23fc9af8dc
Jassiel Rodriguez <hazer94@gmail.com>
2020-08-04 07:32:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,13 @@ end
 
 introduction
 
+def introduction_with_language (name, language)
+  puts "H, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional (name, language = ruby)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+
 
 
 
